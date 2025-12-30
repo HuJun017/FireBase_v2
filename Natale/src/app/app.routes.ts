d65b1198352b22fb0d288a1fc4b676ad97e60f12
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-12-30 18:29:15 +0000

    aggiunti nuovi path

--- Natale/src/app/app.routes.ts
+++ Natale/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Home } from './pages/home/home';
 import { authGuard } from './auth/auth-guard';
 import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
+import { Mesg } from './pages/mesg/mesg';
+import { Library } from './pages/library/library';
+import { Profilo } from './pages/profilo/profilo';
 
 export const routes: Routes = [
     //sintassi di authGuard
@@ -10,5 +13,8 @@ export const routes: Routes = [
     canActivate: [authGuard] },
     { path: 'login', component: Login },
     { path: 'register', component: Register },
+    { path: 'mesg', component: Mesg },
+    { path: 'library', component: Library },
+    { path: 'profile', component: Profilo },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
