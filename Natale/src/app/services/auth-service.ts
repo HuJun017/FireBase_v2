dd294794abf0eaa6c87e836ac59298f51c600a10
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2026-01-16 13:56:40 +0000

    implementazione salvataggio utente nel database

--- Natale/src/app/services/auth-service.ts
+++ Natale/src/app/services/auth-service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { User, Auth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 import { BehaviorSubject } from 'rxjs';
+import { Firestore, doc, getDoc, setDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthService {
   // BehaviorSubject che contiene l'utente corrente (User o null).
   // Serve per far sapere al resto dell'app se qualcuno è loggato.
@@ -13,12 +15,15 @@ export class AuthService {
   // Observable che altri componenti possono "ascoltare".
   currentUser$ = this.currentUserSubject.asObservable();
 
-  constructor(private auth: Auth) {
+  constructor(private auth: Auth, private firestore: Firestore) {
     // onAuthStateChanged ascolta i cambiamenti di login/logout.
     // Viene chiamato automaticamente quando un utente fa login, logout
     // o quando la pagina viene ricaricata e Firebase ripristina la sessione.
     onAuthStateChanged(this.auth, (user) => {
       this.currentUserSubject.next(user);
+      if (user) {
+        this.createUserInFirestoreIfNotExists(user);
+      }
     });
   }
 
@@ -41,4 +46,21 @@ export class AuthService {
   getCurrentUser(): User | null {
     return this.auth.currentUser;
   }
+
+  private async createUserInFirestoreIfNotExists(user: User) {
+
+    const userRef = doc(this.firestore, `users/${user.uid}`);
+    const snapshot = await getDoc(userRef);
+
+    if (!snapshot.exists()) {
+      await setDoc(userRef, {
+        uid: user.uid,
+        email: user.email,
+        provider: user.providerData[0]?.providerId,
+        createdAt: new Date()
+      });
+    }
+
+  }
 }
+
